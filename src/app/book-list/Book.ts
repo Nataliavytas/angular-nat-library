8a3b72e6b99f8d3018cd9509e6523885941fce1d
Natalia Vytas <natyjazvytas@gmail.com>
2020-10-24 17:49:59 -0300

    add book interface and beer-list component

---
+++ src/app/book-list/Book.ts
@@ -0,0 +1,7 @@
+export interface Book {
+    name: string;
+    gender: string;
+    price: number; 
+    stock: number;
+    clearence: boolean;
+}
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from './Book';
 
 @Component({
   selector: 'app-book-list',
@@ -7,12 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListComponent implements OnInit {
 
-  book = {
-    "name" : "Silverville",
-    "gender": "Contemporaneo",
-    "price": 21,
-    "stock": 4
+  books: Book[] = [{
+    name : "Silverville",
+    gender: "Contemporaneo",
+    price: 321,
+    stock: 4,
+    clearence: false,
+  },
+  {
+    name : "Matilde debe morir",
+    gender: "Contemporaneo",
+    price: 221,
+    stock: 2,
+    clearence: true,
+  },
+  {
+    name : "Cazadores de Sombras",
+    gender: "Contemporaneo",
+    price: 20,
+    stock: 0,
+    clearence: false,
   }
+]
   constructor() { }
 
   ngOnInit(): void {
